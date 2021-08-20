bf3db97470ed9a4d6d35b7ea12271870e14bf370
Ranjeet Singh Paliwal <ranjeet.paliwal@timesinternet.in>
2021-08-20 20:31:32 +0530

    under dev

--- src/Pages/FundRequest/FundRequest.js
+++ src/Pages/FundRequest/FundRequest.js
@@ -1,158 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import SideBar from "../../Components/SideBar/SideBar";
 import BreadCrumb from "../../Components/BreadCrumb/BreadCrumb";
 import "./FundRequest.css";
 
 const FundRequest = (props) => {
+  const [isPopupVisible, handlePopUp] = useState(false);
+
+  const fundRequestClickHandler = () => {
+    document.body.classList.add("modal-open");
+    handlePopUp(true);
+  };
+
+  const closePopUpHandler = () => {
+    document.body.classList.remove("modal-open");
+    handlePopUp(false);
+  };
+
   return (
     <div className="container_full">
       <SideBar {...props} />
       <div className="content_wrapper">
         <div className="container-fluid">
           <BreadCrumb heading="Fund Request" value="Fund Request" />
-          <section class="chart_section">
-            <div class="card card-shadow mb-4">
-              <div class="card-header fund-modal">
-                <div class="card-title">Fund Request</div>
+          <section className="chart_section">
+            <div className="card card-shadow mb-4">
+              <div className="card-header fund-modal">
+                <div className="card-title">Fund Request</div>
                 <button
                   type="button"
-                  class="btn btn-secondary fund-btn"
+                  className="btn btn-secondary fund-btn"
                   data-toggle="modal"
                   data-target="#exampleModal"
+                  onClick={fundRequestClickHandler}
                 >
                   Fund Request
                 </button>
 
-                <div
-                  class="modal fade"
-                  id="exampleModal"
-                  tabindex="-1"
-                  role="dialog"
-                  aria-labelledby="exampleModalLabel"
-                  aria-hidden="true"
-                >
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">
-                          Fund Request
-                        </h5>
-                        <button
-                          type="button"
-                          class="close"
-                          data-dismiss="modal"
-                          aria-label="Close"
-                        >
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <form class="">
-                          <div class="row">
-                            <div class="col-md-6">
-                              <div class="form-group">
-                                <label for="exampleFormControlSelect1">
-                                  Choose Bank
-                                </label>
-                                <select
-                                  class="form-control"
-                                  id="exampleFormControlSelect1"
-                                >
-                                  <option>ICICI</option>
-                                  <option>SBI</option>
-                                  <option>HDFC</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div class="col-md-6">
-                              <div class="form-group">
-                                <label for="exampleInputEmail1">
-                                  Deposit Account
-                                </label>
-                                <input
-                                  type="text"
-                                  class="form-control"
-                                  id="depositAccount"
-                                  aria-describedby="Deposit Account"
-                                  placeholder="Deposit Account"
-                                />
-                              </div>
-                            </div>
-                            <div class="col-md-12">
-                              <div class="form-group">
-                                <label for="exampleFormControlSelect1">
-                                  Choose Payment Mode
-                                </label>
-                                <select
-                                  class="form-control"
-                                  id="exampleFormControlSelect1"
-                                >
-                                  <option>Online</option>
-                                  <option>Offline</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div class="col-md-6">
-                              <div class="form-group">
-                                <label for="exampleInputEmail1">
-                                  Requested Amount
-                                </label>
-                                <input
-                                  type="text"
-                                  class="form-control"
-                                  id="requestedAmount"
-                                  aria-describedby="requestedAmount"
-                                  placeholder="Requested Amount"
-                                />
-                              </div>
-                            </div>
-                            <div class="col-md-6">
-                              <div class="form-group">
-                                <label for="exampleFormControlSelect1">
-                                  Wallet Type
-                                </label>
-                                <select class="form-control" id="walletType">
-                                  <option>Prepaid</option>
-                                  <option>Postpaid</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div class="col-md-12">
-                              <div class="form-group">
-                                <label for="exampleInputEmail1">
-                                  Upload Image
-                                </label>
-                                <label class="custom-file">
-                                  <input
-                                    type="file"
-                                    id="file2"
-                                    class="custom-file-input"
-                                  />
-                                  <span class="custom-file-control"></span>
-                                </label>
-                              </div>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
-                      <div class="modal-footer">
-                        <button
-                          type="button"
-                          class="btn btn-primary themebtn"
-                          data-dismiss="modal"
-                        >
-                          Close
-                        </button>
-                        <button type="submit" class="btn btn-primary themebtn">
-                          Submit
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <FundRequestPopUp
+                  isPopupVisible={isPopupVisible}
+                  closePopUpHandler={closePopUpHandler}
+                />
               </div>
-              <div class="card-body">
-                <table class="table table-bordered">
+              <div className="card-body">
+                <table className="table table-bordered">
                   <thead>
                     <tr>
                       <th scope="col">#</th>
@@ -180,14 +70,14 @@ const FundRequest = (props) => {
                       <td>
                         <img
                           src="http://0.0.0.0:3008/images/img2.jpg"
-                          class="img-circle mCS_img_loaded"
+                          className="img-circle mCS_img_loaded"
                           alt="User Image"
                         />
                       </td>
                       <td>
                         <img
                           src="http://0.0.0.0:3008/images/img2.jpg"
-                          class="img-circle mCS_img_loaded"
+                          className="img-circle mCS_img_loaded"
                           alt="User Image"
                         />
                       </td>
@@ -204,14 +94,14 @@ const FundRequest = (props) => {
                       <td>
                         <img
                           src="http://0.0.0.0:3008/images/img2.jpg"
-                          class="img-circle mCS_img_loaded"
+                          className="img-circle mCS_img_loaded"
                           alt="User Image"
                         />
                       </td>
                       <td>
                         <img
                           src="http://0.0.0.0:3008/images/img2.jpg"
-                          class="img-circle mCS_img_loaded"
+                          className="img-circle mCS_img_loaded"
                           alt="User Image"
                         />
                       </td>
@@ -228,14 +118,14 @@ const FundRequest = (props) => {
                       <td>
                         <img
                           src="http://0.0.0.0:3008/images/img2.jpg"
-                          class="img-circle mCS_img_loaded"
+                          className="img-circle mCS_img_loaded"
                           alt="User Image"
                         />
                       </td>
                       <td>
                         <img
                           src="http://0.0.0.0:3008/images/img2.jpg"
-                          class="img-circle mCS_img_loaded"
+                          className="img-circle mCS_img_loaded"
                           alt="User Image"
                         />
                       </td>
@@ -252,14 +142,14 @@ const FundRequest = (props) => {
                       <td>
                         <img
                           src="http://0.0.0.0:3008/images/img2.jpg"
-                          class="img-circle mCS_img_loaded"
+                          className="img-circle mCS_img_loaded"
                           alt="User Image"
                         />
                       </td>
                       <td>
                         <img
                           src="http://0.0.0.0:3008/images/img2.jpg"
-                          class="img-circle mCS_img_loaded"
+                          className="img-circle mCS_img_loaded"
                           alt="User Image"
                         />
                       </td>
@@ -276,3 +166,130 @@ const FundRequest = (props) => {
 };
 
 export default FundRequest;
+
+const FundRequestPopUp = ({ isPopupVisible, closePopUpHandler }) => {
+  const style = isPopupVisible ? { display: "block" } : { display: "none" };
+
+  return (
+    <div
+      className={`modal fade${isPopupVisible ? " show" : ""}`}
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+      style={style}
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Fund Request
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onClick={closePopUpHandler}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <form className="">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label for="exampleFormControlSelect1">Choose Bank</label>
+                    <select
+                      className="form-control"
+                      id="exampleFormControlSelect1"
+                    >
+                      <option>ICICI</option>
+                      <option>SBI</option>
+                      <option>HDFC</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label for="exampleInputEmail1">Deposit Account</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="depositAccount"
+                      aria-describedby="Deposit Account"
+                      placeholder="Deposit Account"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <label for="exampleFormControlSelect1">
+                      Choose Payment Mode
+                    </label>
+                    <select
+                      className="form-control"
+                      id="exampleFormControlSelect1"
+                    >
+                      <option>Online</option>
+                      <option>Offline</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label for="exampleInputEmail1">Requested Amount</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="requestedAmount"
+                      aria-describedby="requestedAmount"
+                      placeholder="Requested Amount"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label for="exampleFormControlSelect1">Wallet Type</label>
+                    <select className="form-control" id="walletType">
+                      <option>Prepaid</option>
+                      <option>Postpaid</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <label for="exampleInputEmail1">Upload Image</label>
+                    <label className="custom-file">
+                      <input
+                        type="file"
+                        id="file2"
+                        className="custom-file-input"
+                      />
+                      <span className="custom-file-control"></span>
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-primary themebtn"
+              data-dismiss="modal"
+              onClick={closePopUpHandler}
+            >
+              Close
+            </button>
+            <button type="submit" className="btn btn-primary themebtn">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
