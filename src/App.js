49a5c027f1d0adc17ffe216a1158c2e49d91aa47
Ranjeet Singh Paliwal <ranjeet.paliwal@timesinternet.in>
2021-09-18 21:08:09 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+//import "../public/css/style.css";
 import React, { Component, Fragment } from "react";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
