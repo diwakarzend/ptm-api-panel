b38be1f01c753083314d4c24669d357f2922ced8
Ranjeet Singh Paliwal <ranjeet.paliwal@timesinternet.in>
2021-09-30 11:24:16 +0530

    dev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./assests/css/style.css";
+import "./assests/css/themify-icons.css";
+import "./assests/css/jquery.mCustomScrollbar.css";
+import "./assests/css/simple-line-icons.css";
+import "./assests/css/ionicons.css";
+
 //import "../public/css/style.css";
 import React, { Component, Fragment } from "react";
 import Header from "./Components/Header/Header";
