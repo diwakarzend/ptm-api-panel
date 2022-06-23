6f371925419b48bfe52b8499706487346adcb90d
Ashutosh <asutosh.sing@pay2mobiles.com>
2022-06-23 17:24:42 -0400

    ui fixes for dashboard

--- src/Components/Dashboard/PtmVendor/style.js
+++ src/Components/Dashboard/PtmVendor/style.js
@@ -4,15 +4,14 @@ export const UpiCollectionsStyle = styled.div`
   .collection-info {
     border: 1px solid #c4c4c4;
     box-shadow: 0 0 5px 0 #0aaaba;
-    padding: 8px 16px;
+    padding: 16px 16px 8px 0;
     border-radius: 12px;
     display: flex;
-    align-items: center;
     & + .collection-info {
       margin-top: 30px;
     }
     .info-left {
-      flex: 0 0 60px;
+      flex: 0 0 80px;
       border-right: 2px solid #c4c4c4;
       display: flex;
       align-items: center;
