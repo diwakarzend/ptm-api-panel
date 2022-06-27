d24ea6474258a868533b641d6894cdaa755fe017
diwakar@pay2mobiles.com <diwakarUpadhyay@Diwakars-MacBook-Pro-2.local>
2022-06-27 16:05:33 +0530

    changes

--- public/build/runtime-233148a89d34d1907537.js
+++ public/build/runtime-233148a89d34d1907537.js
@@ -155,7 +155,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("72cacce5834018f24491")
+/******/ 		__webpack_require__.h = () => ("233148a89d34d1907537")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
